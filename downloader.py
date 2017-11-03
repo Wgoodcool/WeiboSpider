391af8fcd677d6481928a64d633d6bbbb11e6f10
Wzzzx <wzx10.06@qq.com>
2017-11-03 10:41:14 +0800

    Update downloader.py
    1.增加日志记录模块
    2.改用多进程+协程进行数据下载

--- downloader.py
+++ downloader.py
@@ -2,11 +2,14 @@ from middle.transmission import Response
 from middle.middlequeue import uidQueue, requestQueue
 from middle.middlequeue import responseQueue, errorQueue, userResponseQueue
 from middle.middlequeue import proxyQueue
+from middle.middlequeue import logQueue
 from middle.settings import cookie#, UserAgent
 from middle.settings import ProcessNumber, uidGeventProcessNumber, infoGeventProcessNumber
 from middle.settings import maxuidCoroutineNum, maxinfoCoroutineNum
 from manager import InfoManager
 
+import logging
+from logging.handlers import QueueHandler
 import requests
 from multiprocessing import Process, Queue
 import random
@@ -25,70 +28,78 @@ class Downloader:
         self.qerror = errorQueue
         self.quser = userResponseQueue
         self.qproxy = proxyQueue
-        #方便传参
-        self.uidQueueList = [self.quid, self.quser, self.qproxy]
-        self.resQueueList = [self.qresquest, self.qresponse, self.qerror, self.qproxy]
+        #日志队列
+        self.logqueue = logQueue
 
         #通过队列的阻塞来控制启动的最大进程数
         self.ProcessNumberQueue = Queue(ProcessNumber)    
+
         #控制gevent一次处理的Request数  
         self.uidGeventProcessNumber = uidGeventProcessNumber
         self.infoGeventProcessNumber = infoGeventProcessNumber
 
-        self.uidNumberQueue = [self.ProcessNumberQueue, self.uidGeventProcessNumber]
-        self.infoNumberQueue = [self.ProcessNumberQueue, self.infoGeventProcessNumber]
-            
+    def getLog(self, name):
+        print ('getLog')
+        logger = logging.getLogger(name)
+        queue_handler = QueueHandler(self.logqueue)
+        logger.addHandler(queue_handler)
+
+        return logger
+
     def Start(self):
         print ('Is Downloader')
+        log = self.getLog('Downloader')
+        log.warning('Start')
+        uid = Process(target = self.userRequestManager)
+        com = Process(target = self.infoRequestManager)
 
-        uid = Process(target = self.userRequestManager, args = (self.uidQueueList, self.uidNumberQueue))
-        com = Process(target = self.infoRequestManager, args = (self.resQueueList, self.infoNumberQueue))
-
-        print ('Downloader Start')
         uid.start()
         com.start()
         uid.join()
         com.join()
 
-    def userRequestManager(self, QueueList, maxValue):
+        log.warning('End!')
+
+    def userRequestManager(self):
         print ('userRequestManager')
-        self.uidReqQueue = QueueList[0]
-        req = self.uidReqQueue.get()
+        log = self.getLog('userRequestManager')
+        log.warning('Start')
+        req = self.quid.get()
         requestList = []
         while req:
             requestList.append(req)
-            req = self.uidReqQueue.get()
-            if len(requestList) == maxValue[1]:
+            req = self.quid.get()
+            if len(requestList) == self.uidGeventProcessNumber:
                 p = Process(target = self.userGevent,
-                                args = (requestList, QueueList, maxValue[0]))
+                                args = (requestList, ))
                 p.start()
                 requestList = []
-            #达到一定大小会阻塞，除非userRequest完成所有的抓取工作
-            maxValue[0].get()
+                #达到一定大小会阻塞，除非userRequest完成所有的抓取工作
+                self.ProcessNumberQueue.put(0)
+                log.warning('Create Process')
 
-    def userGevent(self, requestList, QueueList, maxNumQueue):
+    def userGevent(self, requestList):
         print ('userGevent')
+        log = self.getLog('Downloader.userGevent')
+        log.warning('Start')
         self.manager = InfoManager()
         self.manager.start()
-        self.sem = Semaphore(maxuidCoroutineNum)
         self.db_proxy = self.manager.proxyOpe()
-        self.uqueue = QueueList[0]
-        self.resqueue = QueueList[1]
-        self.proxy = QueueList[2]
+        self.sem = Semaphore(maxuidCoroutineNum)
 
         task = []
         for req in requestList:
             task.append(gevent.spawn(self.userFetch, req))
         gevent.joinall(task)
-        maxNumQueue.put(0)
+        log.warning('All End')
+        self.ProcessNumberQueue.get()
 
     def userFetch(self, req):
-        print ('userFetch')
         url = req.url
         cate = req.category
         meta = req.meta
         header = self.GetHeader(cate)
-#        proxy = self.proxy.get()
+#        proxy = self.qproxy.get()
         proxy = None
 
         try:
@@ -97,56 +108,52 @@ class Downloader:
             print (type(req))
         except requests.exceptions.ConnectionError as pe:
             #self.db_proxy.delIp(proxy)
-            self.uqueue.put(req)
+            self.quid.put(req)
         finally:
             self.sem.release()
 
         # 没有返回有用数据
         if (len(req.text) < 500) or req.status_code != 200:
             print ('Downloader Fail')
-            self.uqueue.put(req)
+            self.quid.put(req)
 
         res = Response(url, cate, req.text, meta)
-        self.resqueue.put(res)
+        self.quser.put(res)
 
-        req = self.uqueue.get()
-
-    def infoRequestManager(self, QueueList, maxValue):
+    def infoRequestManager(self):
         print ('infoRequestManager')
-        self.infoQueue = QueueList[0]
+        log = self.getLog('Downloader.infoRequestManager')
+        log.warning('Start')
         infoReqList = []
-
-        req = self.infoQueue.get()
+        req = self.qresquest.get()
         while req:
             infoReqList.append(req)
-            req = self.infoQueue.get()
-            if len(infoReqList) == maxValue[1]:
+            req = self.qresquest.get()
+            if len(infoReqList) == self.infoGeventProcessNumber:
                 p = Process(target = self.infoGevent,
-                                args = (infoReqList, QueueList, maxValue[0]))
+                                args = (infoReqList, ))
                 p.start()
                 infoReqList = []
-            maxValue[0].get()
+                self.ProcessNumberQueue.put(0)
 
-    def infoGevent(self, infoReqList, QueueList, maxNum):
+    def infoGevent(self, infoReqList):
+        log = self.getLog('Downloader.infoGevent')
+        log.warning('Start')
         #一次允许并发的最大协程数
         self.sem = Semaphore(maxinfoCoroutineNum)
-        print ("infoGevent")
         self.manager = InfoManager()
         self.manager.start()
         self.db_proxy = self.manager.proxyOpe()
-        self.requeue = QueueList[0]
-        self.resqueue = QueueList[1]
-        self.equeue = QueueList[2]
-        self.pro = QueueList[3]
 
         task = []
         for req in infoReqList:
             task.append(gevent.spawn(self.infoFetch, req))
         gevent.joinall(task)
-        maxNum.put(0)
+
+        log.warning('Process End')
+        self.ProcessNumberQueue.get()
 
     def infoFetch(self, req):
-        print ('infoFetch')
         url = req.url
         cate = req.categoty
         meta = req.meta
@@ -154,25 +161,26 @@ class Downloader:
 # =============================================================================
 #             测试暂时不需要代理
 # =============================================================================
-        # proxy = self.pro.get()
+        # proxy = self.qproxy.get()
         proxy = None
 
         try:
             self.sem.acquire()
             req = requests.get(url, headers = header, proxies = proxy)
         except requests.exceptions.ConnectionError as pe:
-            self.equeue.put(req)
-            self.db_proxy.delIp(proxy)
+            self.qerror.put(req)
+            # self.db_proxy.delIp(proxy)
         finally:
             self.sem.release()
 
         # 没有返回有用数据
         #代理池增加一个字段，此处失败一次，该字段自增1.大于一定值删除
         if (len(req.text) < 500) or req.status_code != 200:
-            self.equeue.put(req)
+            self.qerror.put(req)
+            self.log.warning('Fail')
         else:
             res = Response(url, cate, req.text, meta)
-            self.resqueue.put(res)
+            self.qresponse.put(res)
 
     def GetHeader(self, cate):
         #header的发送顺序问题
