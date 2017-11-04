d0a7c877c1380c90b8c3c2bbc464212aac3f7520
Wzzzx <wzx10.06@qq.com>
2017-11-04 23:09:55 +0800

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,40 +1,56 @@
 # -*- coding: utf-8 -*-
 from middle.transmission import Request
 from middle.middlequeue import requestQueue, uidQueue
-from manager import InfoManager
+from middle.middlequeue import logQueue
+from database import UserOpe
+from middle.settings import GetIDNumber
 
 from math import ceil
 import random
 import time
+import logging
+from logging.handlers import QueueHandler
 
 class Schedule:
     def __init__(self):
         self.uidQueue = uidQueue
         self.requestQueue = requestQueue
-# =============================================================================
-# TODO: getId()可以控制一次返回的数量，不用频繁的读写数据库
-# =============================================================================
+        self.initUrl()
+        #日志队列
+        self.logqueue = logQueue
+
+    def getLog(self, name):
+        logger = logging.getLogger(name)
+        queue_handler = QueueHandler(self.logqueue)
+        logger.addHandler(queue_handler)
+
+        return logger
+
 #种子用户需稍微多些，以免不够用
     def Start(self):
-        self.manager = InfoManager()
-        self.manager.start()
-        self.db_user = self.manager.UserOpe()
-
-        self.initUrl()
-
+        #manager不能放在__init__初始化？？？？
+        # self.manager = InfoManager()
+        # self.manager.start()
+        # self.db_user = self.manager.UserOpe()
+        self.db_user = UserOpe()
         info = self.db_user.getInfo()
-        print ('Is Schedule')
+
+        self.log = self.getLog('Schedule')
+        self.log.warning('Start')
+
         while True:
             if info:
                 self.CreateInfoRequest(info)
-                uid = self.db_user.getId(10)
+                uid = self.db_user.getId(GetIDNumber)
                 self.CreateUidRequest(uid)
-                info = self.db_user.getInfo()
+                self.log.warning('Finish ' + str(info[0]))
             else:
                 print ('sleep')
-                time.sleep(random.randint(10, 15))
-                uid = self.db_user.getId(5)
+                #等待时间
+                time.sleep(random.randint(3, 5))
+                uid = self.db_user.getId(GetIDNumber)
                 self.CreateUidRequest(uid)
+            info = self.db_user.getInfo()
 
     def CreateUidRequest(self, uset):
         print ('CreateUidRequest')
@@ -56,44 +72,63 @@ class Schedule:
         self.pageOffans = ceil(info[2] / 20)
         self.pageOffol = ceil(info[3] / 30)
 
+    def CreatedPageNum(self, num):
+        for n in range(1, num+1):
+            yield n
+
+#   将各类请求随机取段，封装成request，加入requestQueue,
     def CreateInfoRequest(self, info):
         self.fillPage(info)
         fa_url = self.fans_url.format(self.uid)
         fo_url = self.fol_url.format(self.uid)
         mb_url = self.blog_url.format(self.uid)
 
-#   将各类请求随机取段，封装成request，加入requestQueue, 随机取段算法可能有误
-        blog_start = fan_start = fol_start = 1
-        pageSum = self.pageOffans + self.pageOffol + self.pageOfmblog
-        while (blog_start + fan_start + fol_start <= pageSum):
-            mb = blog_start < self.pageOfmblog and (random.randint(3, 5) + blog_start) or blog_start
-            fa = fan_start < self.pageOffans and (random.randint(3, 5) + fan_start) or fan_start
-            fo = fol_start < self.pageOffol and (random.randint(3, 5) + fol_start) or fol_start
-            mb = (mb > self.pageOfmblog) and self.pageOfmblog or mb
-            fa = (fa > self.pageOffans) and self.pageOffol or fa
-            fo = (fa > self.pageOffol) and self.pageOffol or fo
+        kind = [2, 3, 4]
+        mblog = self.CreatedPageNum(self.pageOfmblog)
+        fans = self.CreatedPageNum(self.pageOffans)
+        follows = self.CreatedPageNum(self.pageOffol)
+        while kind.__len__():
 
-            for n in range(fan_start, self.pageOffans + 1):
-                if n == fa:
-                    blog_start = fa
+            if kind.__len__() != 1:
+                choice = random.choice(kind)
+                run = random.randint(3, 5)
+            else:
+                choice = kind[0]
+                run = 99999
+
+            if choice == 4:
+                try:
+                    for n in range(run):
+                        page = next(mblog)
+                        req = Request((fa_url % page), category = 4)
+                        self.requestQueue.put(req)
+                except StopIteration as st:
+                    kind.remove(4)
                     break
-                req = Request((mb_url % n), 
-                                category = 2, meta = {'uid' : self.uid})
-                self.requestQueue.put(req)
-            for n in range(fol_start, self.pageOffol + 1):
-                if n == fo:
-                    blog_start = fo
+            elif choice == 2:
+                try:
+                    for n in range(run):
+                        page = next(fans)
+                        req = Request((mb_url % page), 
+                                        category = 2, meta = {'uid' : self.uid})
+                        self.requestQueue.put(req)                      
+                except StopIteration as st:
+                    kind.remove(2)
                     break
-                req = Request((fo_url % n), 
-                                category = 3, meta = {'uid' : self.uid})
-                self.requestQueue.put(req)
-            for n in range(blog_start, self.pageOfmblog + 1):
-                if n == mb:
-                    blog_start = mb
+            else:
+                try:
+                    for n in range(run):
+                        page = next(follows)
+                        req = Request((fo_url % page), 
+                                        category = 3, meta = {'uid' : self.uid})
+                        self.requestQueue.put(req)
+                except StopIteration as st:
+                    kind.remove(3)
                     break
-                req = Request((fa_url % n), category = 4)
-                self.requestQueue.put(req)                    
-        req = Request(self.detail_url.format(self.uid), category = 1)
+
+        req = Request(self.detail_url.format(self.uid), category = 1, 
+                                                meta = {'uid' : self.uid})
+        print ('CreateInfoRequest ', self.uid)
         self.requestQueue.put(req)       
 
 if __name__ == "__main__":
