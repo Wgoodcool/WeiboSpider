29d1dd1e9b6519f70c7411a7a24658f89672b328
Wzzzx <wzx10.06@qq.com>
2017-11-04 22:53:53 +0800

    Update and rename queue.py to middlequeue.py

--- middle/middlequeue.py
+++ middle/middlequeue.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-
 from multiprocessing import Queue
 
 #Schedule给Downloader传输用
-requestQueue = Queue()
+requestQueue = Queue(50)
 #downloader给spider传输普通response用
 responseQueue = Queue()
 #schedule给downloader传输用户ID请求用
-uidQueue = Queue()
+uidQueue = Queue(50)
 #downloader给spider传输用户初次信息用
 userResponseQueue = Queue()
 #下载错误队列
 errorQueue = Queue()
 #proxy通道
-proxyQueue = Queue(10)
+proxyQueue = Queue(100)
+#日志队列
+logQueue = Queue()
